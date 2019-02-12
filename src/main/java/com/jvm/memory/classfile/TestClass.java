4c7d55b1a199f5f13b5476f3b496d8bb238eec7d
gyoomi <gyoomi0709@foxmail.com>
2019-02-12 18:48:37 +0800

    class文件结构

---
+++ src/main/java/com/jvm/memory/classfile/TestClass.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright © 2019, LeonKeh
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.jvm.memory.classfile;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2019/2/12 14:23
+ */
+public class TestClass {
+
+    private int m;
+
+    public int incr() {
+        return m + 1;
+    }
+
+}
