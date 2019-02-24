27b06f3bbc8c951dea981cd2884b042812c1c2d7
leon <gyoomi0709@foxmail.com>
2019-02-24 18:55:53 +0800

    基于栈的字节码解释执行引擎

---
+++ src/main/java/com/jvm/memory/invokebytecode/InvokeDemo.java
@@ -0,0 +1,17 @@
+package com.jvm.memory.invokebytecode;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2019/2/24 18:47
+ */
+public class InvokeDemo {
+
+    public int calc() {
+        int a = 100;
+        int b = 200;
+        int c = 300;
+        return (a + b) * c;
+    }
+}
